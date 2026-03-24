148c46bd7bc0ae146f76e2ee53d7b6a5f11eb69f
Jayden-Briffa <jaydenbriffa@outlook.com>
2026-03-24 09:07:53 +0000

    Set up Vue scaffold

---
+++ vue-project/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+
+import Header from "@/components/Header.vue"
+import Footer from "@/components/Footer.vue"
+</script>
+
+<template>
+  <Header/>
+
+  <main>
+
+  </main>
+
+  <Footer/>
+</template>
+
+<style scoped>
+  * {
+    background-color: yellow;
+  }
+</style>
---
+++ vue-project/src/components/Footer.vue
@@ -0,0 +1,16 @@
+<script setup>
+    import {ref} from "vue"
+    const year = ref("2026")
+</script>
+
+<template>
+    <footer>©{{year}}</footer>
+</template>
+
+<style>
+    footer {
+        text-align: center;
+        width: 100%;
+        background-color: red
+    }
+</style>
