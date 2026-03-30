c0a1ccaf1876619356229dc8c2d4c29b67a931e7
Jayden-Briffa <jaydenbriffa@outlook.com>
2026-03-30 23:02:59 +0100

    Complete tutorial

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,22 +1,9 @@
 <script setup>
-import { ref } from 'vue';
-
-  const name = ref("John doe")
-  const status = ref("")
-  const tasks = ref(["Task 1", "Task 2", "Task 3"])
-  const href = "https://google.com"
+import Navbar from '@/components/Navbar.vue'
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
-  <h1>Hello, {{ name }}</h1>
-  <p v-if="status == 'active'">Active</p>
-  <p v-else-if="status == 'pending'">User is pending</p>
-  <p v-else>Inactive</p>
-
-
-  <h1>Tasks:</h1>
-  <ul>
-    <li v-for="task in tasks" :key="task">{{ task }}</li>
-  </ul>
-  <a :href>Google!</a>
+  <Navbar />
+  <RouterView />
 </template>
---
+++ vue-project/src/components/Card.vue
@@ -0,0 +1,22 @@
+<script setup> 
+import { computed } from 'vue' 
+const props = defineProps({ 
+    bg: { 
+        type: String, 
+        default: "gray" 
+    } 
+})
+
+const bgClass = computed(() => { 
+    return { 
+        gray: "bg-gray-100", 
+        green: "bg-green-100",
+    }[props.bg] }) 
+    
+</script>
+
+<template>
+    <div :class="[bgClass, 'p-6 rounded-lg shadow-md']"> 
+        <slot /> 
+    </div> 
+</template>
