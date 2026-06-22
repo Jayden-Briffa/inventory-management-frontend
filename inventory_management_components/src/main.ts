1b5fbf813120dd9cfbb53e68ac0460193f93e4d6
jayden-briffa <jayden.briffa@vodafone.com>
2026-06-22 14:44:50 +0100

    implement toast

--- inventory_management_components/src/main.ts
+++ inventory_management_components/src/main.ts
@@ -4,9 +4,11 @@ import './assets/utilities.scss'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import Toast from 'primevue/toast';
 
 const app = createApp(App)
 
 app.use(router)
+app.use(Toast)
 
 app.mount('#app')
